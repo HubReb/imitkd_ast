e1900b140302e534a64554867fc579620d3e246d
HubReb <hubert@cl.uni-heidelberg.de>
2022-11-11 06:52:48 +0100

    remove old_fashioned_train functions; simplify process; check if folders exist

--- fairseq/examples/speech_to_text/get_source_text.py
+++ fairseq/examples/speech_to_text/get_source_text.py
@@ -8,7 +8,7 @@ from pathlib import Path
 
 def get_column_text(filename, column_name="tgt_txt"):
     """get text data in one column"""
-    df = pd.read_csv(filename, sep="\t")
+    df = pd.read_csv(filename, sep="\t", error_bad_lines=False)
     if column_name == "tgt_txt":
         text = "\n".join(df.tgt_text.tolist())
     elif column_name == "src_txt":
@@ -50,11 +50,9 @@ def add_tokenized_text_to_target(filename, tokenized_text, result_filename, colu
 def get_source_text_old_fashioned(filename, column_name="tgt_txt", column_number=6):
     """ to be used if pandas refuses to recognizes seperators"""
     if column_name == "tgt_txt":
-        index = 4
-        if column_number != 6:
-            index = 3
+        index = 3
     elif column_name == "src_txt":
-        index = -1
+        index = -2
     else:
         raise ValueError(f"{column_name} is not supported")
     with open(filename) as f:
@@ -67,143 +65,118 @@ def add_tokenized_text_to_target_old_fashioned(filename, tokenized_text, result_
     """ Replace non-preprocessed text with pre-processed text for either target or source: To be used if csv cannot be loaded with pandas"""
     with open(filename) as f:
         data = f.read().split("\n")[:-1]
-    if column_name == "tgt_txt":
-        new_csv = [data[0]]
-    elif column_name == "src_txt":
-        new_csv = [data[0]  + "\t" + "src_text"]
-    else:
-        raise ValueError(f"{column_name} is not supported")
+    new_csv = [data[0]]
     tokenized_text = tokenized_text.split("\n")[:-1]
     for i, line in enumerate(data[1:]):
         line = line.split("\t")
         if column_name == "tgt_txt":
             new_csv.append("\t".join(line[:3]).strip() + "\t" + tokenized_text[i] + "\t" + "\t".join(line[4:]).strip())
         else:
-            new_csv.append("\t".join(line[:5]).strip() + "\t" + tokenized_text[i])
-    with open(result_filename, "w") as f:
-        f.write("\n".join(new_csv))
-
-
-def add_tokenized_text_to_target_old_fashioned_train(filename, tokenized_text, result_filename, column_name="tgt_txt"):
-    """ Replace non-preprocessed text with pre-processed text for either target or source: To be used if csv cannot be loaded with pandas (special format required for MUST-C train"""
-    with open(filename) as f:
-        data = f.read().split("\n")[:-1]
-    if column_name == "tgt_txt":
-        new_csv = ["\t".join(data[0].split("\t")[:3] + data[0].split("\t")[4:])]
-    elif column_name == "src_txt":
-        new_csv = [data[0]]
-    else:
-        raise ValueError(f"{column_name} is not supported")
-    tokenized_text = tokenized_text.split("\n")[:-1]
-    for i, line in enumerate(data[1:]):
-        line = line.split("\t")
-        if column_name == "tgt_txt":
-            new_csv.append("\t".join(line[:3]).strip() + "\t" + tokenized_text[i] + "\t" + "\t".join(line[5:]).strip())
-        else:
-            new_csv.append("\t".join(line[:5]).strip() + "\t" + tokenized_text[i].strip())
+            new_csv.append("\t".join(line[:4]).strip() + "\t" + tokenized_text[i] + "\t" +  "\t".join(line[5:]).strip())
     with open(result_filename, "w") as f:
         f.write("\n".join(new_csv))
 
 
 def get_text_from_datasets():
     """ Extract src and target text for all files """
-    target_text = get_column_text("covost/en/train_st_en_de_with_source_text.tsv")
-    save_text_in_file("covost/train_text.txt", target_text)
-    target_text = get_column_text("covost/en/dev_st_en_de_with_source_text.tsv")
-    save_text_in_file("covost/dev_text.txt", target_text)
-    target_text = get_column_text("covost/en/test_st_en_de_with_source_text.tsv")
-    save_text_in_file("covost/test_text.txt", target_text)
-    target_text = get_column_text("covost/en/dev_st_en_de_with_source_text.tsv", column_name="src_txt")
-    save_text_in_file("covost/dev_text_en.txt", target_text)
-    target_text = get_column_text("covost/en/test_st_en_de_with_source_text.tsv", column_name="src_txt")
-    save_text_in_file("covost/test_text_en.txt", target_text)
-    target_text = get_source_text_old_fashioned("mustc_csvs/train_st_with_source_text.tsv")
+    if not Path("mustc_processed/").exists:
+        import os
+        os.makedirs("mustc_processed/")
+    target_text = get_source_text_old_fashioned("/home/rebekka/t2b/Projekte/ma/test_preprocessing/imitkd_ast/fairseq/MUSTC_data/en-de//train_st_with_source_text.tsv")
     save_text_in_file("mustc_processed/train_text.txt", target_text)
-    target_text = get_source_text_old_fashioned("mustc_csvs/dev_st_with_source_text.tsv", column_number=5)
+    target_text = get_column_text("/home/rebekka/t2b/Projekte/ma/test_preprocessing/imitkd_ast/fairseq/MUSTC_data/en-de//dev_st_with_source_text.tsv")
     save_text_in_file("mustc_processed/dev_text.txt", target_text)
-    target_text = get_column_text("mustc_csvs/tst-COMMON_st_with_source_text.tsv")
+    target_text = get_column_text("/home/rebekka/t2b/Projekte/ma/test_preprocessing/imitkd_ast/fairseq/MUSTC_data/en-de//tst-COMMON_st_with_source_text.tsv")
     save_text_in_file("mustc_processed/test_text.txt", target_text)
-    target_text = get_source_text_old_fashioned("mustc_csvs/train_st_with_source_text_normal.tsv", column_name="src_txt")
+    target_text = get_source_text_old_fashioned("/home/rebekka/t2b/Projekte/ma/test_preprocessing/imitkd_ast/fairseq/MUSTC_data/en-de//train_st_with_source_text.tsv", column_name="src_txt")
     save_text_in_file("mustc_processed/train_text_en.txt", target_text)
-    target_text = get_column_text("mustc_processed/dev_processed.tsv", column_name="src_txt")
+    target_text = get_column_text("/home/rebekka/t2b/Projekte/ma/test_preprocessing/imitkd_ast/fairseq/MUSTC_data/en-de//dev_st_with_source_text.tsv", column_name="src_txt")
     save_text_in_file("mustc_processed/dev_text_en.txt", target_text)
-    target_text = get_column_text("mustc_processed/tst-COMMON_processed.tsv", column_name="src_txt")
+    target_text = get_column_text("/home/rebekka/t2b/Projekte/ma/test_preprocessing/imitkd_ast/fairseq/MUSTC_data/en-de/tst-COMMON_st_with_source_text.tsv", column_name="src_txt")
     save_text_in_file("mustc_processed/test_text_en.txt", target_text)
-    source_text = get_column_text("covost/en/train_st_en_de_with_source_text_normal.tsv", column_name="src_txt")
-    save_text_in_file("covost/train_text_en.txt", source_text)
+    if not Path("covost_processed/").exists:
+        import os
+        os.makedirs("covost_processed/")
+    target_text = get_source_text_old_fashioned("/home/rebekka/t2b/Projekte/ma/test_preprocessing/imitkd_ast/fairseq/covost_data/en/train_st_with_source_text.tsv")
+    save_text_in_file("covost_processed/train_text.txt", target_text)
+    target_text = get_column_text("/home/rebekka/t2b/Projekte/ma/test_preprocessing/imitkd_ast/fairseq/covost_data/en/dev_st_with_source_text.tsv")
+    save_text_in_file("covost_processed/dev_text.txt", target_text)
+    target_text = get_column_text("/home/rebekka/t2b/Projekte/ma/test_preprocessing/imitkd_ast/fairseq/covost_data/en/tst-COMMON_st_with_source_text.tsv")
+    save_text_in_file("covost_processed/test_text.txt", target_text)
+    target_text = get_source_text_old_fashioned("/home/rebekka/t2b/Projekte/ma/test_preprocessing/imitkd_ast/fairseq/covost_data/en/train_st_with_source_text.tsv", column_name="src_txt")
+    save_text_in_file("covost_processed/train_text_en.txt", target_text)
+    target_text = get_column_text("/home/rebekka/t2b/Projekte/ma/test_preprocessing/imitkd_ast/fairseq/covost_data/en/dev_st_with_source_text.tsv", column_name="src_txt")
+    save_text_in_file("covost_processed/dev_text_en.txt", target_text)
+    target_text = get_column_text("/home/rebekka/t2b/Projekte/ma/test_preprocessing/imitkd_ast/fairseq/covost_data/entst-COMMON_st_with_source_text.tsv", column_name="src_txt")
+    save_text_in_file("covost_processed/test_text_en.txt", target_text)
 
 
 def create_processed_datasets():
-    """ Replace sourc and target text with pre-processed source and target for all files """
-    
-    gen_vocab(Path("covost_processed_text/train.tok.de"), Path("covost/en/spm_bpe8000_ast"), "bpe", 8000)
-
-    tokenized_training_text = get_tokenized_text("covost_processed_text/train.tok.de")
-    add_tokenized_text_to_target('covost/en/train_st_en_de_with_source_text.tsv', tokenized_training_text,
-                                 'covost/en/train_processed.tsv')
-    tokenized_training_text = get_tokenized_text("covost_processed_text/train_asr.tok.en")
-    add_tokenized_text_to_target("covost/en/train_asr_en.tsv", tokenized_training_text,
-                                 "covost/en/train_asr_processed.tsv", column_name="tgt_txt")
-    tokenized_dev_text = get_tokenized_text("covost_processed_text/dev.tok.de")
-    add_tokenized_text_to_target('covost/en/dev_st_en_de_with_source_text.tsv', tokenized_dev_text,
-                                 'covost/en/dev_processed.tsv')
-    tokenized_dev_text = get_tokenized_text("covost_processed_text/dev_asr.tok.en")
-    add_tokenized_text_to_target('covost/en/dev_asr_en.tsv', tokenized_dev_text,
-                                 'covost/en/dev_asr_processed.tsv', column_name="tgt_txt")
-    tokenized_test_text = get_tokenized_text('covost_processed_text/test.tok.de')
-    add_tokenized_text_to_target('covost/en/test_st_en_de_with_source_text.tsv', tokenized_test_text,
-                                 'covost/en/test_processed.tsv')
-    tokenized_test_text = get_tokenized_text('covost_processed_text/test_asr.tok.en')
-    add_tokenized_text_to_target('covost/en/test_asr_en.tsv', tokenized_test_text,
-                                 'covost/en/test_asr_processed.tsv', column_name="tgt_txt")
-   
-    gen_vocab(Path("mustc_processed_text/train.tok.de"), Path("MUST/en-de/spm_bpe8000_ast"), "bpe", 8000)
+    """ Replace source and target text with pre-processed source and target for all files """
     tokenized_training_text = get_tokenized_text("mustc_processed_text/train.tok.de")
-    add_tokenized_text_to_target_old_fashioned_train("mustc_csvs/train_st_with_source_text.tsv", tokenized_training_text,
+    add_tokenized_text_to_target_old_fashioned("/home/rebekka/t2b/Projekte/ma/test_preprocessing/imitkd_ast/fairseq/MUSTC_data/en-de//train_st_with_source_text.tsv", tokenized_training_text,
                                                      "mustc_processed/train_processed.tsv")
     tokenized_dev_text = get_tokenized_text("mustc_processed_text/dev.tok.de")
-    add_tokenized_text_to_target_old_fashioned("mustc_csvs/dev_st_with_source_text.tsv", tokenized_dev_text,
+    add_tokenized_text_to_target("/home/rebekka/t2b/Projekte/ma/test_preprocessing/imitkd_ast/fairseq/MUSTC_data/en-de//dev_st_with_source_text.tsv", tokenized_dev_text,
                                                "mustc_processed/dev_processed.tsv")
     tokenized_test_text = get_tokenized_text("mustc_processed_text/test.tok.de")
-    add_tokenized_text_to_target_old_fashioned("mustc_csvs/tst-COMMON_st_with_source_text.tsv", tokenized_test_text,
+    add_tokenized_text_to_target("/home/rebekka/t2b/Projekte/ma/test_preprocessing/imitkd_ast/fairseq/MUSTC_data/en-de//tst-COMMON_st_with_source_text.tsv", tokenized_test_text,
                                                "mustc_processed/tst-COMMON_processed.tsv")
     tokenized_training_text = get_tokenized_text("mustc_processed_text/train.tok.en")
-    add_tokenized_text_to_target_old_fashioned_train("mustc_csvs/train_asr.tsv", tokenized_training_text,
+    add_tokenized_text_to_target_old_fashioned("/home/rebekka/t2b/Projekte/ma/test_preprocessing/imitkd_ast/fairseq/MUSTC_data/en-de//train_asr.tsv", tokenized_training_text,
                                                      "mustc_processed/train_asr_processed.tsv")
-    tokenized_dev_text = get_tokenized_text("mustc_processed_text/dev.tok.en")
-    add_tokenized_text_to_target_old_fashioned("mustc_csvs/dev_asr.tsv", tokenized_dev_text,
-                                               "mustc_processed/dev_asr_processed.tsv")
-    tokenized_test_text = get_tokenized_text("mustc_processed_text/test.tok.en")
-    add_tokenized_text_to_target_old_fashioned("mustc_csvs/tst-COMMON_asr.tsv", tokenized_test_text,
-                                               "mustc_processed/tst-COMMON_asr_processed.tsv")
-
     tokenized_training_text = get_tokenized_text("mustc_processed_text/train.tok.en")
     add_tokenized_text_to_target_old_fashioned("mustc_processed/train_processed.tsv", tokenized_training_text,
                                                "mustc_processed/train_processed.tsv", column_name="src_txt")
 
     tokenized_dev_text = get_tokenized_text("mustc_processed_text/dev.tok.en")
-    add_tokenized_text_to_target_old_fashioned("mustc_processed/dev_processed.tsv", tokenized_dev_text,
+    add_tokenized_text_to_target("mustc_processed/dev_processed.tsv", tokenized_dev_text,
                                                "mustc_processed/dev_processed.tsv", column_name="src_txt")
     tokenized_test_text = get_tokenized_text("mustc_processed_text/test.tok.en")
-    add_tokenized_text_to_target_old_fashioned("mustc_processed/tst-COMMON_processed.tsv", tokenized_test_text,
+    add_tokenized_text_to_target("mustc_processed/tst-COMMON_processed.tsv", tokenized_test_text,
                                                "mustc_processed/tst-COMMON_processed.tsv", column_name="src_txt")
-    tokenized_training_text = get_tokenized_text("../../../v1.1/en/de/train/segments.tok.en")
-    add_tokenized_text_to_target_old_fashioned("../../../v1.1/train_st_en_de.tsv", tokenized_training_text,
-                                               "../../../v1.1/train_processed.tsv", column_name="src_txt")
-    tokenized_train_noisy_text = get_tokenized_text("../../../v1.1/en/de/train-noisy/segments.tok.en")
-    add_tokenized_text_to_target_old_fashioned("../../../v1.1/train-noisy_st_en_de.tsv", tokenized_train_noisy_text,
-                                               "../../../v1.1/train-noisy_processed.tsv", column_name="src_txt")
-    tokenized_test_text = get_tokenized_text("../../../v1.1/en/de/dev/segments.tok.en")
-    add_tokenized_text_to_target_old_fashioned("../../../v1.1/dev_st_en_de.tsv", tokenized_test_text,
-                                               "../../../v1.1/dev_processed.tsv", column_name="src_txt")
-    tokenized_test_text = get_tokenized_text("../../../v1.1/en/de/test/segments.tok.en")
-    add_tokenized_text_to_target_old_fashioned("../../../v1.1/test_st_en_de.tsv", tokenized_test_text,
-                                               "../../../v1.1/test_processed.tsv", column_name="src_txt")
+    tokenized_dev_text = get_tokenized_text("mustc_processed_text/dev.tok.en")
+    add_tokenized_text_to_target_old_fashioned("/home/rebekka/t2b/Projekte/ma/test_preprocessing/imitkd_ast/fairseq/MUSTC_data/en-de//dev_asr.tsv", tokenized_dev_text,
+                                               "mustc_processed/dev_asr_processed.tsv")
+    tokenized_test_text = get_tokenized_text("mustc_processed_text/test.tok.en")
+    add_tokenized_text_to_target("/home/rebekka/t2b/Projekte/ma/test_preprocessing/imitkd_ast/fairseq/MUSTC_data/en-de//tst-COMMON_asr.tsv", tokenized_test_text,
+                                               "mustc_processed/tst-COMMON_asr_processed.tsv")
+
+
+    tokenized_training_text = get_tokenized_text("covost_processed_text/train.tok.de")
+    add_tokenized_text_to_target_old_fashioned("/home/rebekka/t2b/Projekte/ma/test_preprocessing/imitkd_ast/fairseq/covost_data/en/train_st_with_source_text.tsv", tokenized_training_text,
+                                                     "covost_processed/train_processed.tsv")
+    tokenized_dev_text = get_tokenized_text("covost_processed_text/dev.tok.de")
+    add_tokenized_text_to_target("/home/rebekka/t2b/Projekte/ma/test_preprocessing/imitkd_ast/fairseq/covost_data/en/dev_st_with_source_text.tsv", tokenized_dev_text,
+                                               "covost_processed/dev_processed.tsv")
+    tokenized_test_text = get_tokenized_text("covost_processed_text/test.tok.de")
+    add_tokenized_text_to_target("/home/rebekka/t2b/Projekte/ma/test_preprocessing/imitkd_ast/fairseq/covost_data/en/test_st_with_source_text.tsv", tokenized_test_text,
+                                               "covost_processed/test_processed.tsv")
+    tokenized_training_text = get_tokenized_text("covost_processed_text/train.tok.en")
+    add_tokenized_text_to_target_old_fashioned("/home/rebekka/t2b/Projekte/ma/test_preprocessing/imitkd_ast/fairseq/covost_data/en/train_asr.tsv", tokenized_training_text,
+                                                     "covost_processed/train_asr_processed.tsv")
+    tokenized_training_text = get_tokenized_text("covost_processed_text/train.tok.en")
+    add_tokenized_text_to_target_old_fashioned("covost_processed/train_processed.tsv", tokenized_training_text,
+                                               "covost_processed/train_processed.tsv", column_name="src_txt")
+
+    tokenized_dev_text = get_tokenized_text("covost_processed_text/dev.tok.en")
+    add_tokenized_text_to_target("covost_processed/dev_processed.tsv", tokenized_dev_text,
+                                               "covost_processed/dev_processed.tsv", column_name="src_txt")
+    tokenized_test_text = get_tokenized_text("covost_processed_text/test.tok.en")
+    add_tokenized_text_to_target("covost_processed/test_processed.tsv", tokenized_test_text,
+                                               "covost_processed/test_processed.tsv", column_name="src_txt")
+    tokenized_dev_text = get_tokenized_text("covost_processed_text/dev.tok.en")
+    add_tokenized_text_to_target_old_fashioned("/home/rebekka/t2b/Projekte/ma/test_preprocessing/imitkd_ast/fairseq/covost_data/en/dev_asr.tsv", tokenized_dev_text,
+                                               "covost_processed/dev_asr_processed.tsv")
+    tokenized_test_text = get_tokenized_text("covost_processed_text/test.tok.en")
+    add_tokenized_text_to_target("/home/rebekka/t2b/Projekte/ma/test_preprocessing/imitkd_ast/fairseq/covost_data/en/test_asr.tsv", tokenized_test_text,
+                                               "covost_processed/test_asr_processed.tsv")
+
 
 
 if __name__ == "__main__":
     if not Path("covost_processed_text/train.tok.de").is_file():
         get_text_from_datasets()
         raise OSError("pre-processing with moses-tokenizer and perl script is required - run prepare-rest.sh before "
-                      "continuing")
+                       "continuing")
     create_processed_datasets()
