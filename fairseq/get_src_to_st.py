2553d06d8d6437ea52d006a9c720abb9c0400fd5
HubReb <hubert@cl.uni-heidelberg.de>
2022-11-11 06:51:37 +0100

    simplify process; remove unneeded functions

--- fairseq/get_src_to_st.py
+++ fairseq/get_src_to_st.py
@@ -1,12 +1,36 @@
 #!/usr/bin/env python3 
 
 import pandas as pd
+import numpy as np
+
+
+
+def get_src_in_frame_on_join(tgt_tsv, src_tsv):
+    src_df = pd.read_csv(src_tsv, sep="\t")
+    tgt_df = pd.read_csv(tgt_tsv, sep="\t")
+    src_df.id = src_df.id.str.strip()
+    tgt_df.id = tgt_df.id.str.strip()
+    tgt_df.audio = tgt_df.audio.str.strip()
+    src_df.audio = src_df.audio.str.strip()
+    tgt_df.speaker = tgt_df.speaker.str.strip()
+    src_df.speaker = src_df.speaker.str.strip()
+    tgt_df = tgt_df[tgt_df['id'].isin(src_df['id'])]
+    src_df = src_df[src_df['id'].isin(tgt_df['id'])]
+    df = pd.merge(tgt_df, src_df, on=["id", "audio", "n_frames", "speaker"], how='left')
+    # new_df = pd.merge(tgt_df, src_df, on=["id", "audio", "speaker", "n_frames"], how="inner")
+    # new_df = tgt_df.set_index("id").join(src_df.set_index("id"), on="id", lsuffix='_caller')
+    df.columns = ['src_text' if x=='tgt_text_y' else x for x in df.columns]
+    df.columns = ['tgt_text' if x=='tgt_text_x' else x for x in df.columns]
+    df.to_csv(f"{tgt_tsv.split('.tsv')[0]}_with_source_text.tsv", sep="\t", index=False)
 
 
 def get_src_in_frame(tgt_tsv, src_tsv):
     src_df = pd.read_csv(src_tsv, sep="\t")
     tgt_df = pd.read_csv(tgt_tsv, sep="\t")
     id_list = tgt_df.id.tolist()
+    src_df = src_df[src_df['id'].isin(tgt_df['id'])]
+    tgt_df = tgt_df[tgt_df['id'].isin(src_df['id'])]
+
     src_list = src_df.values.tolist()
     tgt_list = tgt_df.values.tolist()
     source_in_order = ["" for _ in tgt_list]
@@ -19,12 +43,11 @@ def get_src_in_frame(tgt_tsv, src_tsv):
             print(list_item)
             continue
     dataframe = tgt_df.assign(src_text=pd.Series(source_in_order))
-    dataframe.to_csv(f"{tgt_tsv.split('.tsv')[0]}_with_source_text.tsv", sep="\t")
+    dataframe["src_text"].replace('', np.nan)
+    dataframe = dataframe.dropna()
+    dataframe.to_csv(f"{tgt_tsv.split('.tsv')[0]}_with_source_text.tsv", sep="\t", index=False)
 
 
-get_src_in_frame("/scratch/hubert/data/covost/en/test_st_en_de.tsv", "/scratch/hubert/data/covost/en/test_asr_en.tsv")
-get_src_in_frame("/scratch/hubert/data/covost/en/dev_st_en_de.tsv", "/scratch/hubert/data/covost/en/dev_asr_en.tsv")
-get_src_in_frame("/scratch/hubert/data/covost/en/train_st_en_de.tsv", "/scratch/hubert/data/covost/en/train_asr_en.tsv")
-get_src_in_frame("/scratch/hubert/fairs/fairseq/examples/speech_to_text/MUST/en-de/train_st.tsv", "/scratch/hubert/fairs/fairseq/examples/speech_to_text/MUST/en-de/train_asr.tsv")
-get_src_in_frame("/scratch/hubert/fairs/fairseq/examples/speech_to_text/MUST/en-de/dev_st.tsv", "/scratch/hubert/fairs/fairseq/examples/speech_to_text/MUST/en-de/dev_asr.tsv")
-get_src_in_frame("/scratch/hubert/fairs/fairseq/examples/speech_to_text/MUST/en-de/tst-COMMON_st.tsv", "/scratch/hubert/fairs/fairseq/examples/speech_to_text/MUST/en-de/tst-COMMON_asr.tsv")
+get_src_in_frame("/home/rebekka/t2b/Projekte/ma/test_preprocessing/imitkd_ast/fairseq/MUSTC_data/en-de/train_st.tsv", "/home/rebekka/t2b/Projekte/ma/test_preprocessing/imitkd_ast/fairseq/MUSTC_data/en-de/train_asr.tsv")
+get_src_in_frame_on_join("/home/rebekka/t2b/Projekte/ma/test_preprocessing/imitkd_ast/fairseq/MUSTC_data/en-de/dev_st.tsv", "/home/rebekka/t2b/Projekte/ma/test_preprocessing/imitkd_ast/fairseq/MUSTC_data/en-de/dev_asr.tsv")
+get_src_in_frame_on_join("/home/rebekka/t2b/Projekte/ma/test_preprocessing/imitkd_ast/fairseq/MUSTC_data/en-de/tst-COMMON_st.tsv", "/home/rebekka/t2b/Projekte/ma/test_preprocessing/imitkd_ast/fairseq/MUSTC_data/en-de/tst-COMMON_asr.tsv")
